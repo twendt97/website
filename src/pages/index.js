a99aa0cef5860d4b26587ed4e4bdc1e17f09a8b0
Thilo Wendt <thilo.wendt@mobotix.com>
2023-06-29 14:28:22 +0000

    Part 3 tutorial plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <h1>Welcome to my Gatsby site!</h1>
       <p>Following Gatsby tutorial</p>
+      <StaticImage
+        alt='Desert with mountains'
+        src='../images/neom.jpg'></StaticImage>
     </Layout>
   )
 }
